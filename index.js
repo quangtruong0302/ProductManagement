1adaeceb3116fabb3b796efd33a7211272c0c8b2
quangtruong0302 <buiquangtruong170302@gmail.com>
2024-07-29 19:04:58 +0700

    uploads code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+require("dotenv").config();
+const PORT = process.env.PORT;
 const Router = require("./routes/index");
 
 app.set("views", `${__dirname}/views`);
